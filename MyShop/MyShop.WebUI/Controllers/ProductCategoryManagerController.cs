92e10cb9242d8804e8133e1d2a4ce1c90c27e87b
o7Ghost <33361213+o7Ghost@users.noreply.github.com>
2020-03-14 18:14:29 -0700

    establish connection between microsoft sql and created basic schema

--- MyShop/MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Constract;
+using MyShop.Core.Models;
 using MyShop.DataAccess.InMemory;
 using System;
 using System.Collections.Generic;
@@ -10,11 +11,11 @@ namespace MyShop.WebUI.Controllers
 {
     public class ProductCategoryManagerController : Controller
     {
-        InMemoryRepository<ProductCategory> context;
+        IRepository<ProductCategory> context;
 
-        public ProductCategoryManagerController()
+        public ProductCategoryManagerController(IRepository<ProductCategory> context)
         {
-            context = new InMemoryRepository<ProductCategory>();
+            this.context = new InMemoryRepository<ProductCategory>();
 
         }
         // GET: ProductManager
