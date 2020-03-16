63ff54a060acdbbdcf41fb2bc3b86b27e69e497e
o7Ghost <33361213+o7Ghost@users.noreply.github.com>
2020-03-15 19:29:04 -0700

    added home page and category slide down list

---
+++ MyShop/MyShop.Core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyShop.Core.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
