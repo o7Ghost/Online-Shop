879962724305827ec761690f8da5192ea01ac987
o7Ghost <33361213+o7Ghost@users.noreply.github.com>
2020-05-06 17:46:42 -0700

    added unit testing

---
+++ MyShop/MyShop.WebUI.Tests/Mocks/MockHttpContext.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyShop.WebUI.Tests.Controllers.Mocks
+{
+  
+    public class MockHttpContext : HttpContextBase
+    {
+        private MockRequest request;
+        private MockReponse response;
+        private HttpCookieCollection cookies;
+
+        public MockHttpContext()
+        {
+            cookies = new HttpCookieCollection();
+            this.request = new MockRequest(cookies);
+            this.response = new MockReponse(cookies);
+        }
+
+
+        public override HttpRequestBase Request
+        {
+            get
+            {
+                return request;
+            }
+        }
+
+        public override HttpResponseBase Response
+        {
+            get
+            {
+                return response;
+            }
+        }
+
+    }
+
+    public class MockReponse : HttpResponseBase {
+
+        private readonly HttpCookieCollection cookies;
+
+        public MockReponse(HttpCookieCollection cookies)
+        {
+            this.cookies = cookies;
+
+        }
+
+        public override HttpCookieCollection Cookies
+        {
+            get
+            {
+
+                return cookies;
+            }
+        }
+    }
+
+    public class MockRequest : HttpRequestBase
+    {
+
+        private readonly HttpCookieCollection cookies;
+
+        public MockRequest(HttpCookieCollection cookies)
+        {
+            this.cookies = cookies;
+
+        }
+
+        public override HttpCookieCollection Cookies
+        {
+            get
+            {
+
+                return cookies;
+            }
+        }
+    }
+}
